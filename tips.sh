e15c470a65ad05e5f2ca857078efb996fe7a73f3
Jaakko Rasi <jaakko.rasi@tuni.fi>
2021-02-05 00:18:20 +0200

    Added tip randomiser

--- tips.sh
+++ tips.sh
@@ -2,6 +2,7 @@
 
 #TOTD_HTTP=1 # Configuration - set to 1, if tips from the server ...
 CURTIP=`cat ~/.curtip`
+RANDOM=$$
 
 # If no value in the current index file, start from start
 if [ -z $CURTIP ] ; then
@@ -30,7 +31,7 @@ fi
 echo "================================================"
 echo
 
-NEXT=`expr $CURTIP % $NUMTIPS`
+NEXT=`expr $RANDOM % $NUMTIPS`
 NEXT=`expr $NEXT + 1`
 
 echo $NEXT > ~/.curtip
